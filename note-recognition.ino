5ba0bcac0efbdf2e4e2dad7e47052ca12e0df47a
Lucas <lucas.leblanc@orange.fr>
2023-02-22 09:34:04 +0100

    Sound analyzing

--- note-recognition.ino
+++ note-recognition.ino
@@ -4,10 +4,15 @@
 #include <SD.h>
 #include <SerialFlash.h>
 
+
 // GUItool: begin automatically generated code
-AudioInputI2S            i2s2;           //xy=640,278
-AudioAnalyzeNoteFrequency notefreq1;      //xy=888,347
-AudioConnection          patchCord1(i2s2, 0, notefreq1, 0);
+AudioInputI2S            i2s1;
+// AudioSynthKarplusStrong  string1;
+// AudioOutputI2S            i2s2;
+// AudioAnalyzeNoteFrequency notefreq1;
+AudioAnalyzeRMS rms1;
+AudioConnection          patchCord1(i2s1, 0, rms1, 0);
+// AudioConnection          patchCord2(string1, rms1);
 AudioControlSGTL5000     sgtl5000_1;     //xy=690,481
 // GUItool: end automatically generated code
 
@@ -16,18 +21,22 @@ void setup() {
 	Serial.begin(9600);
 	AudioMemory(8);
 	sgtl5000_1.enable();
-	sgtl5000_1.volume(0.5);
+	sgtl5000_1.volume(0.3);
 	sgtl5000_1.inputSelect(AUDIO_INPUT_MIC);
-	sgtl5000_1.micGain(36);
-	delay(1000);
-	notefreq1.begin(.15);
+	sgtl5000_1.micGain(60);
+	// notefreq1.begin(.15);
 }
 
 void loop() {
-	if (notefreq1.available()) {
+	// string1.noteOn(300, 1.0);
+	if (rms1.available()) {
+		float value = rms1.read();
+		Serial.printf("Value : %f\n", value);
+	}
+	/* if (notefreq1.available()) {
 		float note = notefreq1.read();
 		float prob = notefreq1.probability();
 		Serial.printf("Note: %3.2f | Probability: %.2f\n", note, prob);
-	}
+	} */
 	delay(1000);
 }
