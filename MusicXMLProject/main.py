10a7beabec580fe9b1f6c93c2a4ad100d9e98f9d
Lucas <lucas.leblanc@orange.fr>
2023-02-23 11:15:07 +0100

    Avancement reconnaissance blanches

--- MusicXMLProject/main.py
+++ MusicXMLProject/main.py
@@ -11,10 +11,12 @@ part = Part("Piano")
 score.append(part)
 measure = Measure()
 measureReinit = Measure()
-oldValue = 0
-lastnote = 0
+lastFreqGivenByTeensy = 0
+lastNote = 0
+lastAmpli = 1
 tempo = 80
-timeBetweenNote = 60 / 80 
+timeBetweenNote = (60 / tempo) - 0.05
+d = 1
 
 
 def convertFreqToNote(freq):
@@ -24,33 +26,52 @@ def convertFreqToNote(freq):
     octaveOfTheNote = str(int(noteInInt / 12) - 1)
     return noteInLetter + octaveOfTheNote
 
+
+def createNote(d, freq):
+    note = Note(pitch=convertFreqToNote(freq), duration=d)
+    measure.append(note)
+    part.append(measure)
+    score.export_to_file("test.xml")
+    part.__delitem__(0)
+
 while True:
     with serial.Serial('COM3', 9600, timeout=1) as ser:
         line = ser.readline()
         # print("line =", line)
         
     
-    if line != b'':
-        i = 0
-        value = ""
+    if line != b'': 
         line = str(line)[2:]
         line = line[:len(line)-3]
-        value, proba, ampli = line.split(",")
+        freq, proba, ampli = line.split(",")
 
-        value = float(value)
+        freq = float(freq)
         proba = float(proba)
         ampli = float(ampli)
         
         now = time.time()
 
-        if (abs(value-oldValue) > 10 or abs(now - lastnote) > timeBetweenNote*2) and proba > 0.85 and ampli > 0.09:
-            # print(value)
-            print(line)
-            note = Note(pitch=convertFreqToNote(value), duration=1)
-            measure.append(note)
-            part.append(measure)
-            score.export_to_file("test.xml")
-            part.__delitem__(0)
-            i+=1
-            lastnote = time.time()
-        oldValue = value
+        print(line)
+        if proba > 0.85 and ampli > 0.09:
+            lastFreqNoteRecognized = freq
+
+        if abs(now - lastNote) > timeBetweenNote and proba > 0.85 and ampli > 0.09:
+            print("----------",line)
+
+            # On détecte une nouvelle note jouée (donc on créée celle d'avant)
+            if ampli > lastAmpli: 
+                createNote(d, lastFreqNoteRecognized)
+                print("Note créée")
+                d = 1
+                
+            # On détecte pas une nouvelle note jouée
+            elif ampli <= lastAmpli: 
+                lastFreqNoteRecognized = freq
+                d = 2 
+                print("Note blanche reconnue")
+
+
+            lastNote = time.time()
+        
+        lastAmpli = ampli    
+        lastFreqGivenByTeensy = freq
