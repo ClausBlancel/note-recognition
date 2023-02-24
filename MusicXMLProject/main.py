992d26ebb686bbb382cf52b5276103fc46e82316
Lucas <lucas.leblanc@orange.fr>
2023-02-24 16:51:37 +0100

    YES
    Co-authored-by: bcoeugnet <bcoeugnet@users.noreply.github.com>

--- MusicXMLProject/main.py
+++ MusicXMLProject/main.py
@@ -3,20 +3,37 @@ from pymusicxml import *
 import serial
 import io
 import time
+import xml.etree.ElementTree as ET
 
 listNote = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
 score = Score(title="Read from the Teensy", composer="Teensy")
 
 part = Part("Piano")
 score.append(part)
-measure = Measure()
-measureReinit = Measure()
-lastFreqGivenByTeensy = 0
-lastNote = 0
-lastAmpli = 1
+score.export_to_file("test.xml")
+
+
+tree = ET.parse('test.xml')
+root = tree.getroot()
+
+part = ET.Element("part")
+part.set("id", "P1")
+measure = ET.Element("measure")
+measure.set("number", "1")
+part.append(measure)
+root.append(part)
+
+
 tempo = 80
-timeBetweenNote = (60 / tempo) - 0.05
-d = 1
+timeBetweenNote = (60 / tempo) 
+firstNote = True
+tabFreq = []
+tabAmpli = []
+tabProba = []
+partition = []
+thisNote = 0
+lastAmpli = 0
+blanche = False
 
 
 def convertFreqToNote(freq):
@@ -28,11 +45,48 @@ def convertFreqToNote(freq):
 
 
 def createNote(d, freq):
-    note = Note(pitch=convertFreqToNote(freq), duration=d)
-    measure.append(note)
-    part.append(measure)
-    score.export_to_file("test.xml")
-    part.__delitem__(0)
+    # note = Note(pitch=convertFreqToNote(freq), duration=d)
+    # measure.append(note)
+    # part.append(measure)
+    # score.write_file("test.xml")
+    # part.__delitem__(0)
+   
+    noteStr = convertFreqToNote(freq)
+
+    new_note = ET.Element("note")
+    pitch = ET.Element("pitch")
+    step = ET.Element("step")
+    step.text = noteStr[0:1]
+    octave = ET.Element("octave")
+    octave.text = noteStr[-1]
+    pitch.append(step)
+    pitch.append(octave)
+    duration = ET.Element("duration")
+    duration.text = str(d)  # valeur de duration de la nouvelle note
+    type = ET.Element("type")
+    type.text = "quarter"
+    new_note.append(pitch)
+    new_note.append(duration)
+    new_note.append(type)
+    measure = root.findall("./part/measure")
+    lastMeasure = measure[-1]
+    lastMeasure.append(new_note)
+
+    tree.write("test.xml")
+
+
+def modifierNote(d):
+    notes = root.findall("./part/measure/note")
+    if notes:
+        last_note = notes[-1]
+        last_duration = last_note.find("duration")
+        last_duration.text = "2"  # la nouvelle valeur de duration
+        last_type = last_note.find("type")
+        last_type.text = "half"
+        # Enregistrer les modifications dans le fichier XML
+        tree.write("test.xml")
+    else:
+        print("Le fichier ne contient pas de notes.")
 
 while True:
     with serial.Serial('COM3', 9600, timeout=1) as ser:
@@ -43,35 +97,41 @@ while True:
     if line != b'': 
         line = str(line)[2:]
         line = line[:len(line)-3]
-        freq, proba, ampli = line.split(",")
+        print(line)
+        freq, proba, ampli, peak = line.split(",")
 
         freq = float(freq)
         proba = float(proba)
         ampli = float(ampli)
-        
-        now = time.time()
+        peak = float(peak)
 
-        print(line)
-        if proba > 0.85 and ampli > 0.09:
-            lastFreqNoteRecognized = freq
+        tabFreq.append(freq)
+        tabProba.append(proba)    
+        tabAmpli.append(ampli)
 
-        if abs(now - lastNote) > timeBetweenNote and proba > 0.85 and ampli > 0.09:
-            print("----------",line)
 
-            # On détecte une nouvelle note jouée (donc on créée celle d'avant)
-            if ampli > lastAmpli: 
-                createNote(d, lastFreqNoteRecognized)
-                print("Note créée")
-                d = 1
+        # if len(tabFreq) > 4 and tabAmpli[-2] > tabAmpli[-3] and tabAmpli[-2] > tabAmpli[-1] and tabAmpli[-2] - tabAmpli[-3] >= 0.05:
+        #     if not firstNote:
+        #         duration = round(abs(thisNote - time.time() / timeBetweenNote))
+        #         print(abs(thisNote - time.time() / timeBetweenNote))
+        #         print(duration)
+
+        #         createNote(duration, tabFreq[-1])
                 
-            # On détecte pas une nouvelle note jouée
-            elif ampli <= lastAmpli: 
-                lastFreqNoteRecognized = freq
-                d = 2 
-                print("Note blanche reconnue")
 
-
-            lastNote = time.time()
-        
-        lastAmpli = ampli    
-        lastFreqGivenByTeensy = freq
+        #     thisNote = time.time()
+        #     firstNote = False
+            
+        if abs(thisNote - time.time()) > timeBetweenNote:
+            if proba > 0.9 and peak > 0.8:
+                createNote(1, freq)
+                print("---noire---", freq)
+                thisNote = time.time()
+                lastAmpli = ampli
+                blanche = False
+                
+            elif not blanche and ampli < lastAmpli:
+                modifierNote(2)  # modifier ici
+                print("---blanche---", freq)
+                blanche = True
+                thisNote = time.time()
--- note-recognition.ino
+++ note-recognition.ino
@@ -7,6 +7,7 @@
 //---------------------------------------------------------------------------------------
 AudioAnalyzeNoteFrequency	notefreq;
 AudioAnalyzeRMS					 rms;
+AudioAnalyzePeak				peak;
 AudioInputI2S					i2s1;
 AudioMixer4					   mixer;
 AudioControlSGTL5000		sgtl5000;
@@ -14,6 +15,7 @@ AudioControlSGTL5000		sgtl5000;
 AudioConnection patchCord0(i2s1, 0, mixer, 0);
 AudioConnection patchCord1(mixer, 0, notefreq, 0);
 AudioConnection patchCord2(i2s1, 0, rms, 0);
+AudioConnection patchCord3(i2s1, 0, peak, 0);
 //---------------------------------------------------------------------------------------
 
 void setup() {
@@ -29,10 +31,11 @@ void setup() {
 }
 
 void loop() {
-	if (notefreq.available()) {
+	if (notefreq.available() and rms.available() and peak.available()) {
 		float note = notefreq.read();
 		float prob = notefreq.probability();
 		float amp = rms.read();
-		Serial.printf("%3.2f,%.2f,%.2f\n", note, prob, amp);
+		float highest = peak.read();
+		Serial.printf("%3.2f,%.2f,%.2f,%.2f\n", note, prob, amp, highest);
 	}
 }
